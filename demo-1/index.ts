5adf6f3e47e45ec703cef416b905214f2e0c284f
diego-cum-telus <79596068+diego-cum-telus@users.noreply.github.com>
2024-05-20 02:41:48 -0600

    feat(demo 1): Primitve data types, arrays and tuples

---
+++ demo-1/index.ts
@@ -0,0 +1,18 @@
+// Definicion de tipos primitivos
+let firstName: string = 'Elsa';
+let lastName: string = 'Pato';
+let age: number = 21;
+let isJunior: boolean = true;
+
+// Definicion de user-defined tipo de datos
+let numbers: number[] = [1, 2, 3, 4, NaN];
+let strings: string[] = ['Elsa', 'Pato', 'Pepito'];
+let boleans: boolean[] = [true, false, true, true];
+
+
+let ages2: Array<number> = [21, 22, 23, 24, 25];
+let strings2: Array<string> = ['Elsa', 'Pato', 'Pepito'];
+let boleans2: Array<boolean> = [true, false, true, true];
+
+// Definicion de tuplas
+let person: [string, string, number, boolean] = ['Elsa', 'Pato', 21, true];
